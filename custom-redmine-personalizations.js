6806ad98b750f2ca2ec1d96119204b5f0a82093b
Mike Betler <17320117+betler@users.noreply.github.com>
2019-11-15 13:41:30 +0100

    Update custom-redmine-personalizations.js

--- custom-redmine-personalizations.js
+++ custom-redmine-personalizations.js
@@ -2,10 +2,15 @@
 window.addEventListener("load", () => {
     // Remove progress bar and set a badge with % in text
     $.each($('[class^=progress'), function (index, value) {
-        $(value).removeClass("progress");
-        var progress = value.classList.item(0).split("-")[1];
-        $(value).empty();
-        $(value).html('<div style="width:4em;font-weight:bold;background: #9DB9D5;color:white;padding: 0px 6px 1px 6px;border-radius: 3px;">' + progress + '%</div>');
-    });}
-    , false)
+            $(value).removeClass("progress");
+            var progress = value.classList.item(0).split("-")[1];
+            $(value).empty();
+            $(value).html('<div style="width:4em;font-weight:bold;background: #9DB9D5;color:white;padding: 0px 6px 1px 6px;border-radius: 3px;">' + progress + '%</div>');
+        });
+
+    // Replace text in all td (shorten project names, for example)
+    $.each($('td'), function (index, value) {
+            $(value).html($(value).html().replace("texto 1", "H2 - "));
+        });
+}, false)
 
