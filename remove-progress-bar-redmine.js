000252a46d168267dd8d92473654ea9c0c4add7a
Mike Betler <17320117+betler@users.noreply.github.com>
2019-11-15 13:07:35 +0100

    Updated

--- remove-progress-bar-redmine.js
+++ remove-progress-bar-redmine.js
@@ -1,6 +1,10 @@
-$.each($('[class^=progress'), function (index, value) {
-    $(value).removeClass("progress");
-    var progress = value.classList.item(0).split("-")[1];
-    $(value).empty();
-    $(value).html('<div style="width:4em;font-weight:bold;background: #9DB9D5;color:white;padding: 0px 6px 1px 6px;border-radius: 3px;">' + progress + '%</div>');
-});
+// JS for Redmine & https://mybrowseraddon.com/custom-style-script.html Opera AddOn 
+window.addEventListener("load", () => {
+    $.each($('[class^=progress'), function (index, value) {
+        $(value).removeClass("progress");
+        var progress = value.classList.item(0).split("-")[1];
+        $(value).empty();
+        $(value).html('<div style="width:4em;font-weight:bold;background: #9DB9D5;color:white;padding: 0px 6px 1px 6px;border-radius: 3px;">' + progress + '%</div>');
+    });}
+    , false)
+
