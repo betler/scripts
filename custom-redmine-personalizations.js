dbc0805593139dd9e9cf498a2d88565daa3e7db9
Mike Betler <17320117+betler@users.noreply.github.com>
2019-11-19 10:19:43 +0100

    mejora script

--- custom-redmine-personalizations.js
+++ custom-redmine-personalizations.js
@@ -1,11 +1,15 @@
 // JS for Redmine & https://mybrowseraddon.com/custom-style-script.html Opera AddOn 
 window.addEventListener("load", () => {
     // Remove progress bar and set a badge with % in text
-    $.each($('[class^=progress'), function (index, value) {
+    $.each($('[table.progress'), function (index, value) {
             $(value).removeClass("progress");
             var progress = value.classList.item(0).split("-")[1];
             $(value).empty();
             $(value).html('<div style="width:4em;font-weight:bold;background: #9DB9D5;color:white;padding: 0px 6px 1px 6px;border-radius: 3px;">' + progress + '%</div>');
+        });
+
+    $.each($('p.percent'), function (index, value) {
+            $(value).remove();
         });
 
     // Replace text in all td (shorten project names, for example)
